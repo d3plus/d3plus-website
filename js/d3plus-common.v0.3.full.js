c41f5224278c10e90084b2a9c4d0af13c167e895
Dave Landry <landry.dave@gmail.com>
2016-06-23 14:26:17 -0400

    d3plus-common v0.3.5

--- js/d3plus-common.v0.3.full.js
+++ js/d3plus-common.v0.3.full.js
@@ -4,7 +4,7 @@
 	(factory((global.d3plus_common = global.d3plus_common || {})));
 }(this, function (exports) { 'use strict';
 
-	var version = "0.3.4";
+	var version = "0.3.5";
 
 	/**
 	    @function accessor
