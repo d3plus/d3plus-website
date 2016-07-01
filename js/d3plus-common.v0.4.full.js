790d69ae97970eefe96b1ab0ccddd22765ad9b4d
Dave Landry <landry.dave@gmail.com>
2016-07-01 12:03:11 -0400

    d3plus-common v0.4.1

--- js/d3plus-common.v0.4.full.js
+++ js/d3plus-common.v0.4.full.js
@@ -1,7 +1,7 @@
 /*
-  d3plus-common v0.4.0
+  d3plus-common v0.4.1
   Common functions and methods used across D3plus modules.
-  Copyright (c) 2016 D3plus
+  Copyright (c) 2016 D3plus - https://d3plus.org
   @license MIT
 */
 (function (global, factory) {
