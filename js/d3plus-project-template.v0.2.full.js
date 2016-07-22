de2cc613806cf2e498cb27f4b071327da7f641f2
Dave Landry <landry.dave@gmail.com>
2016-07-22 19:01:26 -0400

    d3plus-project-template v0.2.3

--- js/d3plus-project-template.v0.2.full.js
+++ js/d3plus-project-template.v0.2.full.js
@@ -1,137 +1,130 @@
+/*
+  d3plus-project-template v0.2.3
+  A starter environment for D3plus modules.
+  Copyright (c) 2016 D3plus - https://d3plus.org
+  @license MIT
+*/
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-	typeof define === 'function' && define.amd ? define('d3plus-project-template', ['exports'], factory) :
-	(factory((global.d3plus_project_template = global.d3plus_project_template || {})));
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define('d3plus-project-template', ['exports'], factory) :
+  (factory((global.d3plus = global.d3plus || {})));
 }(this, function (exports) { 'use strict';
 
-	var version = "0.2.2";
+  function ascending(a, b) {
+    return a < b ? -1 : a > b ? 1 : a >= b ? 0 : NaN;
+  }
 
-	function ascending(a, b) {
-	  return a < b ? -1 : a > b ? 1 : a >= b ? 0 : NaN;
-	}
+  function bisector(compare) {
+    if (compare.length === 1) compare = ascendingComparator(compare);
+    return {
+      left: function(a, x, lo, hi) {
+        if (lo == null) lo = 0;
+        if (hi == null) hi = a.length;
+        while (lo < hi) {
+          var mid = lo + hi >>> 1;
+          if (compare(a[mid], x) < 0) lo = mid + 1;
+          else hi = mid;
+        }
+        return lo;
+      },
+      right: function(a, x, lo, hi) {
+        if (lo == null) lo = 0;
+        if (hi == null) hi = a.length;
+        while (lo < hi) {
+          var mid = lo + hi >>> 1;
+          if (compare(a[mid], x) > 0) hi = mid;
+          else lo = mid + 1;
+        }
+        return lo;
+      }
+    };
+  }
 
-	function bisector(compare) {
-	  if (compare.length === 1) compare = ascendingComparator(compare);
-	  return {
-	    left: function(a, x, lo, hi) {
-	      if (lo == null) lo = 0;
-	      if (hi == null) hi = a.length;
-	      while (lo < hi) {
-	        var mid = lo + hi >>> 1;
-	        if (compare(a[mid], x) < 0) lo = mid + 1;
-	        else hi = mid;
-	      }
-	      return lo;
-	    },
-	    right: function(a, x, lo, hi) {
-	      if (lo == null) lo = 0;
-	      if (hi == null) hi = a.length;
-	      while (lo < hi) {
-	        var mid = lo + hi >>> 1;
-	        if (compare(a[mid], x) > 0) hi = mid;
-	        else lo = mid + 1;
-	      }
-	      return lo;
-	    }
-	  };
-	}
+  function ascendingComparator(f) {
+    return function(d, x) {
+      return ascending(f(d), x);
+    };
+  }
 
-	function ascendingComparator(f) {
-	  return function(d, x) {
-	    return ascending(f(d), x);
-	  };
-	}
+  var ascendingBisect = bisector(ascending);
 
-	var ascendingBisect = bisector(ascending);
+  /**
+      @function constant
+      @desc Wraps non-function variables in a simple return function.
+      @param {Array|Number|Object|String} value The value to be returned from the function.
+      @example <caption>this</caption>
+  constant(42);
+      @example <caption>returns this</caption>
+  function() {
+    return 42;
+  }
+  */
+  function constant$1(value) {
+    return function constant() {
+      return value;
+    };
+  }
 
-	/**
-	    @function constant
-	    @desc Wraps non-function variables in a simple return function.
-	    @param {Array|Number|Object|String} value The value to be returned from the function.
-	    @example <caption>this</caption>
-	constant(42);
-	    @example <caption>returns this</caption>
-	function() {
-	  return 42;
-	}
-	*/
-	function constant$1(value) {
-	  return function constant() {
-	    return value;
-	  };
-	}
+  /**
+      A default accessor function.
+      @private
+  */
+  function sampleId(d) {
+    return d.id;
+  }
 
-	/**
-	    A default accessor function.
-	    @private
-	*/
-	function sampleId(d) {
-	  return d.id;
-	}
-
-	/**
-	    @function sample
-	    @desc A sample chainable function. If *data* is specified, immediately draws and returns this sample generator. If *data* is not specified on instantiation, it can be passed/updated after instantiation using the [data](#sample.data) method.
-	    @param {Array} [data = []]
-	    @example <caption>a sample row of data</caption>
-	var data = {"id": "sample"};
-	@example <caption>passed to the generator</caption>
-	sample([data]);
-	@example <caption>creates the following</caption>
-	<html code goes here>
-	@example <caption>this is shorthand for the following</caption>
-	sample().data([data])();
-	@example <caption>which also allows a post-draw callback function</caption>
-	sample().data([data])(function() { alert("draw complete!"); })
-	*/
-	function sample(data) {
-	  if ( data === void 0 ) data = [];
+  /**
+      @function sample
+      @desc A sample chainable function. If *data* is specified, immediately draws and returns this sample generator. If *data* is not specified on instantiation, it can be passed/updated after instantiation using the [data](#sample.data) method.
+      @param {Array} [data = []]
+  */
+  function sample(data) {
+    if ( data === void 0 ) data = [];
 
 
-	  var id = sampleId,
-	      sampleConstant = constant$1("sample");
+    var id = sampleId,
+        sampleConstant = constant$1("sample");
 
-	  /**
-	      The inner return object and draw function that gets assigned the public methods.
-	      @private
-	  */
-	  function sample(callback) {
+    /**
+        The inner return object and draw function that gets assigned the public methods.
+        @private
+    */
+    function sample(callback) {
 
-	    if (callback) setTimeout(callback, 100);
+      if (callback) setTimeout(callback, 100);
 
-	    return sample;
+      return sample;
 
-	  }
+    }
 
-	  /**
-	      @memberof sample
-	      @desc If *value* is specified, sets the id accessor to the specified function and returns this generator. If *value* is not specified, returns the current id accessor.
-	      @param {Function} [*value*]
-	      @example
-	function(d) {
-	  return d.id;
-	}
-	  */
-	  sample.id = function(_) {
-	    return arguments.length ? (id = _, sample) : id;
-	  };
+    /**
+        @memberof sample
+        @desc If *value* is specified, sets the id accessor to the specified function and returns this generator. If *value* is not specified, returns the current id accessor.
+        @param {Function} [*value*]
+        @example
+  function(d) {
+    return d.id;
+  }
+    */
+    sample.id = function(_) {
+      return arguments.length ? (id = _, sample) : id;
+    };
 
-	  /**
-	      @memberof sample
-	      @desc If *value* is specified, sets the accessor to the specified function or value and returns this generator. If *value* is not specified, returns the current accessor.
-	      @param {Function|Number|String} [*value* = "sample"]
-	  */
-	  sample.sampleConstant = function(_) {
-	    return arguments.length ? (sampleConstant = typeof _ === "function" ? _ : constant$1(_), sample) : sampleConstant;
-	  };
+    /**
+        @memberof sample
+        @desc If *value* is specified, sets the accessor to the specified function or value and returns this generator. If *value* is not specified, returns the current accessor.
+        @param {Function|Number|String} [*value* = "sample"]
+    */
+    sample.sampleConstant = function(_) {
+      return arguments.length ? (sampleConstant = typeof _ === "function" ? _ : constant$1(_), sample) : sampleConstant;
+    };
 
-	  return data.length ? sample() : sample;
+    return data.length ? sample() : sample;
 
-	}
+  }
 
-	exports.version = version;
-	exports.sample = sample;
+  exports.sample = sample;
 
-	Object.defineProperty(exports, '__esModule', { value: true });
+  Object.defineProperty(exports, '__esModule', { value: true });
 
 }));
--- js/d3plus-project-template.v0.2.js
+++ js/d3plus-project-template.v0.2.js
@@ -1,82 +1,75 @@
+/*
+  d3plus-project-template v0.2.3
+  A starter environment for D3plus modules.
+  Copyright (c) 2016 D3plus - https://d3plus.org
+  @license MIT
+*/
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('d3plus-common')) :
-	typeof define === 'function' && define.amd ? define('d3plus-project-template', ['exports', 'd3plus-common'], factory) :
-	(factory((global.d3plus_project_template = global.d3plus_project_template || {}),global.d3plus_common));
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('d3plus-common')) :
+  typeof define === 'function' && define.amd ? define('d3plus-project-template', ['exports', 'd3plus-common'], factory) :
+  (factory((global.d3plus = global.d3plus || {}),global.d3plusCommon));
 }(this, function (exports,d3plusCommon) { 'use strict';
 
-	var version = "0.2.2";
+  /**
+      A default accessor function.
+      @private
+  */
+  function sampleId(d) {
+    return d.id;
+  }
 
-	/**
-	    A default accessor function.
-	    @private
-	*/
-	function sampleId(d) {
-	  return d.id;
-	}
-
-	/**
-	    @function sample
-	    @desc A sample chainable function. If *data* is specified, immediately draws and returns this sample generator. If *data* is not specified on instantiation, it can be passed/updated after instantiation using the [data](#sample.data) method.
-	    @param {Array} [data = []]
-	    @example <caption>a sample row of data</caption>
-	var data = {"id": "sample"};
-	@example <caption>passed to the generator</caption>
-	sample([data]);
-	@example <caption>creates the following</caption>
-	<html code goes here>
-	@example <caption>this is shorthand for the following</caption>
-	sample().data([data])();
-	@example <caption>which also allows a post-draw callback function</caption>
-	sample().data([data])(function() { alert("draw complete!"); })
-	*/
-	function sample(data) {
-	  if ( data === void 0 ) data = [];
+  /**
+      @function sample
+      @desc A sample chainable function. If *data* is specified, immediately draws and returns this sample generator. If *data* is not specified on instantiation, it can be passed/updated after instantiation using the [data](#sample.data) method.
+      @param {Array} [data = []]
+  */
+  function sample(data) {
+    if ( data === void 0 ) data = [];
 
 
-	  var id = sampleId,
-	      sampleConstant = d3plusCommon.constant("sample");
+    var id = sampleId,
+        sampleConstant = d3plusCommon.constant("sample");
 
-	  /**
-	      The inner return object and draw function that gets assigned the public methods.
-	      @private
-	  */
-	  function sample(callback) {
+    /**
+        The inner return object and draw function that gets assigned the public methods.
+        @private
+    */
+    function sample(callback) {
 
-	    if (callback) setTimeout(callback, 100);
+      if (callback) setTimeout(callback, 100);
 
-	    return sample;
+      return sample;
 
-	  }
+    }
 
-	  /**
-	      @memberof sample
-	      @desc If *value* is specified, sets the id accessor to the specified function and returns this generator. If *value* is not specified, returns the current id accessor.
-	      @param {Function} [*value*]
-	      @example
-	function(d) {
-	  return d.id;
-	}
-	  */
-	  sample.id = function(_) {
-	    return arguments.length ? (id = _, sample) : id;
-	  };
+    /**
+        @memberof sample
+        @desc If *value* is specified, sets the id accessor to the specified function and returns this generator. If *value* is not specified, returns the current id accessor.
+        @param {Function} [*value*]
+        @example
+  function(d) {
+    return d.id;
+  }
+    */
+    sample.id = function(_) {
+      return arguments.length ? (id = _, sample) : id;
+    };
 
-	  /**
-	      @memberof sample
-	      @desc If *value* is specified, sets the accessor to the specified function or value and returns this generator. If *value* is not specified, returns the current accessor.
-	      @param {Function|Number|String} [*value* = "sample"]
-	  */
-	  sample.sampleConstant = function(_) {
-	    return arguments.length ? (sampleConstant = typeof _ === "function" ? _ : d3plusCommon.constant(_), sample) : sampleConstant;
-	  };
+    /**
+        @memberof sample
+        @desc If *value* is specified, sets the accessor to the specified function or value and returns this generator. If *value* is not specified, returns the current accessor.
+        @param {Function|Number|String} [*value* = "sample"]
+    */
+    sample.sampleConstant = function(_) {
+      return arguments.length ? (sampleConstant = typeof _ === "function" ? _ : d3plusCommon.constant(_), sample) : sampleConstant;
+    };
 
-	  return data.length ? sample() : sample;
+    return data.length ? sample() : sample;
 
-	}
+  }
 
-	exports.version = version;
-	exports.sample = sample;
+  exports.sample = sample;
 
-	Object.defineProperty(exports, '__esModule', { value: true });
+  Object.defineProperty(exports, '__esModule', { value: true });
 
 }));
