323fa8c1c02cc7e09c762f909b8bf6549a9cde65
Dave Landry <landry.dave@gmail.com>
2016-10-18 23:47:07 -0400

    d3plus-tooltip v0.2.0

---
+++ js/d3plus-tooltip.v0.2.js
@@ -0,0 +1,489 @@
+/*
+  d3plus-tooltip v0.2.0
+  A javascript-only tooltip.
+  Copyright (c) 2016 D3plus - https://d3plus.org
+  @license MIT
+*/
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('d3-selection'), require('d3-transition'), require('d3plus-common')) :
+  typeof define === 'function' && define.amd ? define('d3plus-tooltip', ['exports', 'd3-selection', 'd3-transition', 'd3plus-common'], factory) :
+  (factory((global.d3plus = global.d3plus || {}),global.d3Selection,global.d3Transition,global.d3plusCommon));
+}(this, (function (exports,d3Selection,d3Transition,d3plusCommon) { 'use strict';
+
+/**
+    @function prefix
+    @desc Returns the appropriate vendor prefix to use in CSS styles.
+*/
+var prefix = function() {
+  if ("-webkit-transform" in document.body.style) return "-webkit-";
+  else if ("-moz-transform" in document.body.style) return "-moz-";
+  else if ("-ms-transform" in document.body.style) return "-ms-";
+  else if ("-o-transform" in document.body.style) return "-o-";
+  else return "";
+};
+
+/**
+    @class Tooltip
+    @extends BaseClass
+    @desc Creates HTML tooltips in the body of a webpage.
+*/
+var Tooltip = (function (BaseClass$$1) {
+  function Tooltip() {
+
+    BaseClass$$1.call(this);
+    this._background = d3plusCommon.constant("rgba(255, 255, 255, 0.75)");
+    this._body = d3plusCommon.accessor("body", "");
+    this._bodyStyle = {
+      "font-family": "Verdana",
+      "font-size": "10px",
+      "font-weight": "400"
+    };
+    this._border = d3plusCommon.constant("1px solid rgba(0, 0, 0, 0.1)");
+    this._borderRadius = d3plusCommon.constant("2px");
+    this._className = "d3plus-tooltip";
+    this._duration = d3plusCommon.constant(200);
+    this._footer = d3plusCommon.accessor("footer", "");
+    this._footerStyle = {
+      "font-family": "Verdana",
+      "font-size": "10px",
+      "font-weight": "400"
+    };
+    this._height = d3plusCommon.constant("auto");
+    this._id = function (d, i) { return d.id || ("" + i); };
+    this._offset = d3plusCommon.constant(10);
+    this._padding = d3plusCommon.constant("5px");
+    this._pointerEvents = d3plusCommon.constant("auto");
+    this._prefix = prefix();
+    this._tableStyle = {
+      "border-spacing": "0",
+      "width": "100%"
+    };
+    this._tbody = [];
+    this._tbodyStyle = {
+      "font-family": "Verdana",
+      "font-size": "10px",
+      "text-align": "center"
+    };
+    this._thead = [];
+    this._theadStyle = {
+      "font-family": "Verdana",
+      "font-size": "10px",
+      "font-weight": "600",
+      "text-align": "center"
+    };
+    this._title = d3plusCommon.accessor("title", "");
+    this._titleStyle = {
+      "font-family": "Verdana",
+      "font-size": "12px",
+      "font-weight": "600",
+      "padding-bottom": "5px"
+    };
+    this._translate = function (d) { return [d.x, d.y]; };
+    this._width = d3plusCommon.constant("auto");
+
+  }
+
+  if ( BaseClass$$1 ) Tooltip.__proto__ = BaseClass$$1;
+  Tooltip.prototype = Object.create( BaseClass$$1 && BaseClass$$1.prototype );
+  Tooltip.prototype.constructor = Tooltip;
+
+  /**
+      The inner return object and draw function that gets assigned the public methods.
+      @private
+  */
+  Tooltip.prototype.render = function render (callback) {
+    var this$1 = this;
+
+
+    var that = this;
+
+    var tooltips = d3Selection.select("body").selectAll(("." + (this._className)))
+      .data(this._data, this._id);
+
+    var enter = tooltips.enter().append("div")
+      .attr("class", this._className)
+      .style("position", "absolute")
+      .style(((this._prefix) + "transform"), "scale(0)")
+      .style(((this._prefix) + "transform-origin"), "50% 100%");
+
+    var update = tooltips.merge(enter);
+
+    /**
+        Creates DIV elements with a unique class and styles.
+        @private
+    */
+    function divElement(cat) {
+      enter.append("div").attr("class", ("d3plus-tooltip-" + cat));
+      var div = update.select((".d3plus-tooltip-" + cat)).html(that[("_" + cat)]);
+      d3plusCommon.stylize(div, that[("_" + cat + "Style")]);
+    }
+
+    /**
+        Fetches table contents given functions or values.
+        @private
+    */
+    function cellContent(d) {
+      if (typeof d === "function") {
+        var datum = d3Selection.select(this.parentNode.parentNode).datum();
+        return d(datum, that._data.indexOf(datum));
+      }
+      else return d;
+    }
+
+    /**
+        Sets styles for both enter and update.
+        @private
+    */
+    function boxStyles(box) {
+
+      box
+        .style("background", that._background)
+        .style(((that._prefix) + "border-radius"), that._borderRadius)
+        .style("pointer-events", that._pointerEvents)
+        .style("padding", that._padding)
+        .style("width", that._width)
+        .style("height", that._height)
+        .style("border", function(d, i) {
+          var b = d3Selection.select(this).style("border");
+          return b !== "0px none rgb(0, 0, 0)" ? b : that._border(d, i);
+        })
+        .style("top", function(d, i) {
+          return ((that._translate(d, i)[1] - this.offsetHeight - that._offset(d, i)) + "px");
+        })
+        .style("left", function(d, i) {
+          return ((that._translate(d, i)[0] - this.offsetWidth / 2) + "px");
+        });
+
+    }
+
+    divElement("title");
+    divElement("body");
+
+    var tableEnter = enter.append("table").attr("class", "d3plus-tooltip-table");
+    var table = update.select(".d3plus-tooltip-table");
+    d3plusCommon.stylize(table, this._tableStyle);
+
+    tableEnter.append("thead").attr("class", "d3plus-tooltip-thead");
+    var tableHead = update.select(".d3plus-tooltip-thead");
+    d3plusCommon.stylize(tableHead, this._theadStyle);
+    var th = tableHead.selectAll("th").data(this._thead);
+    th.enter().append("th").merge(th).html(cellContent);
+    th.exit().remove();
+
+    tableEnter.append("tbody").attr("class", "d3plus-tooltip-tbody");
+    var tableBody = update.select(".d3plus-tooltip-tbody");
+    d3plusCommon.stylize(tableBody, this._tbodyStyle);
+    var tr = tableBody.selectAll("tr").data(this._tbody);
+    var trEnter = tr.enter().append("tr");
+    tr.exit().remove();
+    var trUpdate = tr.merge(trEnter);
+    var td = trUpdate.selectAll("td").data(function (d) { return d; });
+    td.enter().append("td").merge(td).html(cellContent);
+
+    divElement("footer");
+
+    enter.call(boxStyles);
+
+    var t = d3Transition.transition().duration(this._duration);
+
+    update
+      .attr("id", function (d, i) { return ("d3plus-tooltip-" + (this$1._id(d, i))); })
+      .transition(t)
+        .style(((this._prefix) + "transform"), "scale(1)")
+        .call(boxStyles);
+
+    tooltips.exit()
+      .transition(t)
+        .style(((this._prefix) + "transform"), "scale(0)")
+        .remove();
+
+    if (callback) setTimeout(callback, this._duration + 100);
+
+    return this;
+
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the background accessor to the specified function or string and returns this generator. If *value* is not specified, returns the current background accessor.
+      @param {Function|String} [*value* = "rgba(255, 255, 255, 0.75)"]
+  */
+  Tooltip.prototype.background = function background (_) {
+    return arguments.length ? (this._background = typeof _ === "function" ? _ : d3plusCommon.constant(_), this) : this._background;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the body accessor to the specified function or string and returns this generator. If *value* is not specified, returns the current body accessor.
+      @param {Function|String} [*value*]
+      @example <caption>default accessor</caption>
+function value(d) {
+  return d.body || "";
+}
+  */
+  Tooltip.prototype.body = function body (_) {
+    return arguments.length ? (this._body = typeof _ === "function" ? _ : d3plusCommon.constant(_), this) : this._body;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the body styles to the specified values and returns this generator. If *value* is not specified, returns the current body styles.
+      @param {Object} [*value*]
+      @example <caption>default styles</caption>
+{
+  "font-family": "Verdana",
+  "font-size": "10px",
+  "font-weight": "400"
+}
+  */
+  Tooltip.prototype.bodyStyle = function bodyStyle (_) {
+    return arguments.length ? (this._bodyStyle = Object.assign(this._bodyStyle, _), this) : this._bodyStyle;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the border accessor to the specified function or string and returns this generator. If *value* is not specified, returns the current border accessor.
+      @param {Function|String} [*value* = "1px solid rgba(0, 0, 0, 0.1)"]
+  */
+  Tooltip.prototype.border = function border (_) {
+    return arguments.length ? (this._border = typeof _ === "function" ? _ : d3plusCommon.constant(_), this) : this._border;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the border-radius accessor to the specified function or string and returns this generator. If *value* is not specified, returns the current border-radius accessor.
+      @param {Function|String} [*value* = "2px"]
+  */
+  Tooltip.prototype.borderRadius = function borderRadius (_) {
+    return arguments.length ? (this._borderRadius = typeof _ === "function" ? _ : d3plusCommon.constant(_), this) : this._borderRadius;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the class name to the specified string and returns this generator. If *value* is not specified, returns the current class name.
+      @param {String} [*value* = "d3plus-tooltip"]
+  */
+  Tooltip.prototype.className = function className (_) {
+    return arguments.length ? (this._className = _, this) : this._className;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *data* is specified, sets the data array to the specified array and returns this generator. If *data* is not specified, returns the current data array.
+      @param {Array} [*data* = []]
+  */
+  Tooltip.prototype.data = function data (_) {
+    return arguments.length ? (this._data = _, this) : this._data;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *ms* is specified, sets the duration accessor to the specified function or number and returns this generator. If *ms* is not specified, returns the current duration accessor.
+      @param {Function|Number} [*ms* = 200]
+  */
+  Tooltip.prototype.duration = function duration (_) {
+    return arguments.length ? (this._duration = typeof _ === "function" ? _ : d3plusCommon.constant(_), this) : this._duration;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the footer accessor to the specified function or string and returns this generator. If *value* is not specified, returns the current footer accessor.
+      @param {Function|String} [*value*]
+      @example <caption>default accessor</caption>
+function value(d) {
+  return d.footer || "";
+}
+  */
+  Tooltip.prototype.footer = function footer (_) {
+    return arguments.length ? (this._footer = typeof _ === "function" ? _ : d3plusCommon.constant(_), this) : this._footer;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the footer styles to the specified values and returns this generator. If *value* is not specified, returns the current footer styles.
+      @param {Object} [*value*]
+      @example <caption>default styles</caption>
+{
+  "font-family": "Verdana",
+  "font-size": "10px",
+  "font-weight": "400"
+}
+  */
+  Tooltip.prototype.footerStyle = function footerStyle (_) {
+    return arguments.length ? (this._footerStyle = Object.assign(this._footerStyle, _), this) : this._footerStyle;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the height accessor to the specified function or string and returns this generator. If *value* is not specified, returns the current height accessor.
+      @param {Function|String} [*value* = "auto"]
+  */
+  Tooltip.prototype.height = function height (_) {
+    return arguments.length ? (this._height = typeof _ === "function" ? _ : d3plusCommon.constant(_), this) : this._height;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the id accessor to the specified function or string and returns this generator. If *value* is not specified, returns the current id accessor.
+      @param {Function|String} [*value*]
+      @example <caption>default accessor</caption>
+function value(d, i) {
+  return d.id || "" + i;
+}
+  */
+  Tooltip.prototype.id = function id (_) {
+    return arguments.length ? (this._id = typeof _ === "function" ? _ : d3plusCommon.constant(_), this) : this._id;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the offset accessor to the specified function or number and returns this generator. If *value* is not specified, returns the current offset accessor.
+      @param {Function|Number} [*value* = 10]
+  */
+  Tooltip.prototype.offset = function offset (_) {
+    return arguments.length ? (this._offset = typeof _ === "function" ? _ : d3plusCommon.constant(_), this) : this._offset;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the padding accessor to the specified function or string and returns this generator. If *value* is not specified, returns the current padding accessor.
+      @param {Function|String} [*value* = "5px"]
+  */
+  Tooltip.prototype.padding = function padding (_) {
+    return arguments.length ? (this._padding = typeof _ === "function" ? _ : d3plusCommon.constant(_), this) : this._padding;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the pointer-events accessor to the specified function or string and returns this generator. If *value* is not specified, returns the current pointer-events accessor.
+      @param {Function|String} [*value* = "auto"]
+  */
+  Tooltip.prototype.pointerEvents = function pointerEvents (_) {
+    return arguments.length ? (this._pointerEvents = typeof _ === "function" ? _ : d3plusCommon.constant(_), this) : this._pointerEvents;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the table styles to the specified values and returns this generator. If *value* is not specified, returns the current table styles.
+      @param {Object} [*value*]
+      @example <caption>default styles</caption>
+{
+  "border-spacing": "0",
+  "width": "100%"
+}
+  */
+  Tooltip.prototype.tableStyle = function tableStyle (_) {
+    return arguments.length ? (this._tableStyle = Object.assign(this._tableStyle, _), this) : this._tableStyle;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the contents of the table body to the specified array of functions or strings and returns this generator. If *value* is not specified, returns the current table body data.
+      @param {Array} [*value* = []]
+  */
+  Tooltip.prototype.tbody = function tbody (_) {
+    return arguments.length ? (this._tbody = _, this) : this._tbody;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the table body styles to the specified values and returns this generator. If *value* is not specified, returns the current table body styles.
+      @param {Object} [*value*]
+      @example <caption>default styles</caption>
+{
+  "font-family": "Verdana",
+  "font-size": "10px",
+  "font-weight": "600",
+  "text-align": "center"
+}
+  */
+  Tooltip.prototype.tbodyStyle = function tbodyStyle (_) {
+    return arguments.length ? (this._tbodyStyle = Object.assign(this._tbodyStyle, _), this) : this._tbodyStyle;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the contents of the table head to the specified array of functions or strings and returns this generator. If *value* is not specified, returns the current table head data.
+      @param {Array} [*value* = []]
+  */
+  Tooltip.prototype.thead = function thead (_) {
+    return arguments.length ? (this._thead = _, this) : this._thead;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the table head styles to the specified values and returns this generator. If *value* is not specified, returns the current table head styles.
+      @param {Object} [*value*]
+      @example <caption>default styles</caption>
+{
+  "font-family": "Verdana",
+  "font-size": "10px",
+  "font-weight": "600",
+  "text-align": "center"
+}
+  */
+  Tooltip.prototype.theadStyle = function theadStyle (_) {
+    return arguments.length ? (this._theadStyle = Object.assign(this._theadStyle, _), this) : this._theadStyle;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the title accessor to the specified function or string and returns this generator. If *value* is not specified, returns the current title accessor.
+      @param {Function|String} [*value*]
+      @example <caption>default accessor</caption>
+function value(d) {
+  return d.title || "";
+}
+  */
+  Tooltip.prototype.title = function title (_) {
+    return arguments.length ? (this._title = typeof _ === "function" ? _ : d3plusCommon.constant(_), this) : this._title;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the title styles to the specified values and returns this generator. If *value* is not specified, returns the current title styles.
+      @param {Object} [*value*]
+      @example <caption>default styles</caption>
+{
+  "font-family": "Verdana",
+  "font-size": "12px",
+  "font-weight": "600",
+  "padding-bottom": "5px"
+}
+  */
+  Tooltip.prototype.titleStyle = function titleStyle (_) {
+    return arguments.length ? (this._titleStyle = Object.assign(this._titleStyle, _), this) : this._titleStyle;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the translate accessor to the specified function or array and returns this generator. If *value* is not specified, returns the current translate accessor.
+      @param {Function|Array} [*value*]
+      @example <caption>default accessor</caption>
+function value(d) {
+  return [d.x, d.y];
+}
+  */
+  Tooltip.prototype.translate = function translate (_) {
+    return arguments.length ? (this._translate = typeof _ === "function" ? _ : d3plusCommon.constant(_), this) : this._translate;
+  };
+
+  /**
+      @memberof Tooltip
+      @desc If *value* is specified, sets the width accessor to the specified function or string and returns this generator. If *value* is not specified, returns the current width accessor.
+      @param {Function|String} [*value* = "auto"]
+  */
+  Tooltip.prototype.width = function width (_) {
+    return arguments.length ? (this._width = typeof _ === "function" ? _ : d3plusCommon.constant(_), this) : this._width;
+  };
+
+  return Tooltip;
+}(d3plusCommon.BaseClass));
+
+exports.prefix = prefix;
+exports.Tooltip = Tooltip;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
+//# sourceMappingURL=d3plus-tooltip.js.map
