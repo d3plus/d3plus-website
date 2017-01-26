b5f60dc290005f2ce97067c79b4e9301be4f766c
Dave Landry <landry.dave@gmail.com>
2017-01-26 02:29:24 -0500

    d3plus-dev v0.2.0

---
+++ js/d3plus-dev.v0.2.js
@@ -0,0 +1,20 @@
+/*
+  d3plus-dev v0.2.0
+  A collection of scripts for developing D3plus modules.
+  Copyright (c) 2017 D3plus - https://d3plus.org
+  @license MIT
+*/
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define('d3plus-dev', ['exports'], factory) :
+	(factory((global.d3plus = global.d3plus || {})));
+}(this, (function (exports) { 'use strict';
+
+var pipe = "Ceci n'est pas une |";
+
+exports.pipe = pipe;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
+//# sourceMappingURL=d3plus-dev.js.map
