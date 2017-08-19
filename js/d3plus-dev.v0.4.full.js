133cffaa938982616e1a13c0ff7b409da4b1299f
Dave Landry <landry.dave@gmail.com>
2017-08-19 16:48:31 -0300

    d3plus-dev v0.4.15

--- js/d3plus-dev.v0.4.full.js
+++ js/d3plus-dev.v0.4.full.js
@@ -1,9 +1,68 @@
 /*
-  d3plus-dev v0.4.14
+  d3plus-dev v0.4.15
   A collection of scripts for developing D3plus modules.
   Copyright (c) 2017 D3plus - https://d3plus.org
   @license MIT
 */
+
+if (typeof Object.assign !== "function") {
+  Object.defineProperty(Object, "assign", {
+    value: function assign(target) {
+      "use strict";
+      if (target === null) {
+        throw new TypeError("Cannot convert undefined or null to object");
+      }
+
+      var to = Object(target);
+
+      for (var index = 1; index < arguments.length; index++) {
+        var nextSource = arguments[index];
+
+        if (nextSource !== null) {
+          for (var nextKey in nextSource) {
+            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+              to[nextKey] = nextSource[nextKey];
+            }
+          }
+        }
+      }
+      return to;
+    },
+    writable: true,
+    configurable: true
+  });
+}
+
+if (!Array.prototype.includes) {
+  Object.defineProperty(Array.prototype, "includes", {
+    value: function includes(searchElement, fromIndex) {
+
+      var o = Object(this);
+
+      var len = o.length >>> 0;
+
+      if (len === 0) return false;
+
+      var n = fromIndex | 0;
+
+      var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+      function sameValueZero(x, y) {
+        return x === y || typeof x === "number" && typeof y === "number" && isNaN(x) && isNaN(y);
+      }
+
+      while (k < len) {
+        if (sameValueZero(o[k], searchElement)) {
+          return true;
+        }
+        k++;
+      }
+
+      return false;
+    }
+  });
+}
+
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define('d3plus-dev', ['exports'], factory) :
@@ -12,7 +71,13 @@
 
 var pipe = "Ceci n'est pas une |";
 
+var assign = function () { return Object.assign({}, {test: true}); };
+
+var includes = function () { return [0, 1, 2].includes(1); };
+
 exports.pipe = pipe;
+exports.assign = assign;
+exports.includes = includes;
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
