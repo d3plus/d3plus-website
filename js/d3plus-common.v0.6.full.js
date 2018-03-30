1c15ecf4eee3a5a094cb7f9f0b02b4bed329f473
Dave Landry <landry.dave@gmail.com>
2018-03-30 14:03:14 -0400

    d3plus-common v0.6.34

--- js/d3plus-common.v0.6.full.js
+++ js/d3plus-common.v0.6.full.js
@@ -1,5 +1,5 @@
 /*
-  d3plus-common v0.6.33
+  d3plus-common v0.6.34
   Common functions and methods used across D3plus modules.
   Copyright (c) 2018 D3plus - https://d3plus.org
   @license MIT
@@ -329,9 +329,8 @@ function configPrep(config, type, nest) {
     for (var event in on) {
 
       if ({}.hasOwnProperty.call(on, event) && !event.includes(".") || event.includes(("." + type))) {
-
-        newObj.on[event] = wrapFunction(on[event]);
-
+        var eventName = event.replace("click", "click touchstart");
+        newObj.on[eventName] = wrapFunction(on[event]);
       }
 
     }
