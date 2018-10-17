291c5f6f67a4bcb07f3c6d735e93ecf8bdbed9cc
Dave Landry <landry.dave@gmail.com>
2018-10-17 14:43:48 -0400

    d3plus-plot v0.8.1

--- js/d3plus-plot.v0.8.full.js
+++ js/d3plus-plot.v0.8.full.js
@@ -5004,8 +5004,6 @@ if (!Array.prototype.includes) {
   var saturday = weekday(6);
 
   var sundays = sunday.range;
-  var mondays = monday.range;
-  var thursdays = thursday.range;
 
   var month = newInterval(function(date) {
     date.setDate(1);
@@ -5095,8 +5093,6 @@ if (!Array.prototype.includes) {
   var utcSaturday = utcWeekday(6);
 
   var utcSundays = utcSunday.range;
-  var utcMondays = utcMonday.range;
-  var utcThursdays = utcThursday.range;
 
   var utcMonth = newInterval(function(date) {
     date.setUTCDate(1);
