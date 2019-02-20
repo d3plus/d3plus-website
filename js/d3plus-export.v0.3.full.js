003ac48ec777bc5ac7fe3aca72b6fdcb21b2a85b
Dave Landry <landry.dave@gmail.com>
2019-02-20 14:00:29 -0500

    d3plus-export v0.3.10

--- js/d3plus-export.v0.3.full.js
+++ js/d3plus-export.v0.3.full.js
@@ -1,5 +1,5 @@
 /*
-  d3plus-export v0.3.9
+  d3plus-export v0.3.10
   Export methods for transforming and downloading SVG.
   Copyright (c) 2019 D3plus - https://d3plus.org
   @license MIT
@@ -61,6 +61,147 @@ if (!Array.prototype.includes) {
       return false;
     }
   });
+}
+
+if (!String.prototype.includes) {
+  Object.defineProperty(String.prototype, 'includes', {
+    value: function(search, start) {
+      if (typeof start !== 'number') {
+        start = 0
+      }
+
+      if (start + search.length > this.length) {
+        return false
+      } else {
+        return this.indexOf(search, start) !== -1
+      }
+    }
+  })
+}
+
+if (!Array.prototype.find) {
+  Object.defineProperty(Array.prototype, 'find', {
+    value: function(predicate) {
+     // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      }
+
+      // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      var thisArg = arguments[1];
+
+      // 5. Let k be 0.
+      var k = 0;
+
+      // 6. Repeat, while k < len
+      while (k < len) {
+        // a. Let Pk be ! ToString(k).
+        // b. Let kValue be ? Get(O, Pk).
+        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+        // d. If testResult is true, return kValue.
+        var kValue = o[k];
+        if (predicate.call(thisArg, kValue, k, o)) {
+          return kValue;
+        }
+        // e. Increase k by 1.
+        k++;
+      }
+
+      // 7. Return undefined.
+      return undefined;
+    },
+    configurable: true,
+    writable: true
+  });
+}
+
+if (!String.prototype.startsWith) {
+  Object.defineProperty(String.prototype, 'startsWith', {
+      value: function(search, pos) {
+          pos = !pos || pos < 0 ? 0 : +pos;
+          return this.substring(pos, pos + search.length) === search;
+      }
+  });
+}
+
+if (typeof window !== "undefined") {
+  (function () {
+    var serializeXML = function (node, output) {
+      var nodeType = node.nodeType;
+      if (nodeType === 3) {
+        output.push(node.textContent.replace(/&/, '&amp;').replace(/</, '&lt;').replace('>', '&gt;'));
+      } else if (nodeType === 1) {
+        output.push('<', node.tagName);
+        if (node.hasAttributes()) {
+          [].forEach.call(node.attributes, function(attrNode){
+            output.push(' ', attrNode.item.name, '=\'', attrNode.item.value, '\'');
+          })
+        }
+        if (node.hasChildNodes()) {
+          output.push('>');
+          [].forEach.call(node.childNodes, function(childNode){
+            serializeXML(childNode, output);
+          })
+          output.push('</', node.tagName, '>');
+        } else {
+          output.push('/>');
+        }
+      } else if (nodeType == 8) {
+        output.push('<!--', node.nodeValue, '-->');
+      }
+    }
+
+    Object.defineProperty(SVGElement.prototype, 'innerHTML', {
+      get: function () {
+        var output = [];
+        var childNode = this.firstChild;
+        while (childNode) {
+          serializeXML(childNode, output);
+          childNode = childNode.nextSibling;
+        }
+        return output.join('');
+      },
+      set: function (markupText) {
+        while (this.firstChild) {
+          this.removeChild(this.firstChild);
+        }
+
+        try {
+          var dXML = new DOMParser();
+          dXML.async = false;
+
+          var sXML = '<svg xmlns=\'http://www.w3.org/2000/svg\' xmlns:xlink=\'http://www.w3.org/1999/xlink\'>' + markupText + '</svg>';
+          var svgDocElement = dXML.parseFromString(sXML, 'text/xml').documentElement;
+
+          var childNode = svgDocElement.firstChild;
+          while (childNode) {
+            this.appendChild(this.ownerDocument.importNode(childNode, true));
+            childNode = childNode.nextSibling;
+          }
+        } catch (e) {};
+      }
+    });
+
+    Object.defineProperty(SVGElement.prototype, 'innerSVG', {
+      get: function () {
+        return this.innerHTML;
+      },
+      set: function (markup) {
+        this.innerHTML = markup;
+      }
+    });
+
+  })();
 }
 
 (function (global, factory) {
@@ -10548,19 +10689,19 @@ if (!Array.prototype.includes) {
 	      var s = options.scale * ratio;
 
 	      var data$1 = {
-	        height: height + options.padding * 2 + offsetY,
+	        height: Math.floor(height + options.padding * 2 + offsetY),
 	        loaded: false,
 	        type: "html",
-	        width: width + options.padding * 2 + offsetX,
-	        x: layerX - offsetX,
-	        y: layerY - offsetY
+	        width: Math.floor(width + options.padding * 2 + offsetX),
+	        x: Math.floor(layerX - offsetX),
+	        y: Math.floor(layerY - offsetY)
 	      };
 
 	      var tempCanvas = document.createElement("canvas");
-	      tempCanvas.width = (width + options.padding * 2 + offsetX) * s;
-	      tempCanvas.height = (height + options.padding * 2 + offsetY) * s;
-	      tempCanvas.style.width = ((width + options.padding * 2 + offsetX) * s) + "px";
-	      tempCanvas.style.height = ((height + options.padding * 2 + offsetY) * s) + "px";
+	      tempCanvas.width = data$1.width * s;
+	      tempCanvas.height = data$1.height * s;
+	      tempCanvas.style.width = (data$1.width * s) + "px";
+	      tempCanvas.style.height = (data$1.height * s) + "px";
 
 	      var tempContext = tempCanvas.getContext("2d");
 	      tempContext.scale(s, s);
@@ -11097,7 +11238,7 @@ if (!Array.prototype.includes) {
 	    if (renderOptions.callback) { renderOptions.callback(canvas); }
 
 	    if (["jpg", "png"].includes(options.type)) {
-	      canvas.toBlob(function (blob) { return FileSaver_1(blob, options.filename); });
+	      canvas.toBlob(function (blob) { return FileSaver_1(blob, ((options.filename) + "." + (options.type))); });
 	    }
 	    // else if (options.type === "pdf") {
 
