0883224bd862d62bc5761fdcfb033ef3242483ee
Dave Landry <landry.dave@gmail.com>
2019-03-08 13:04:55 -0500

    d3plus-react v0.5.3

--- js/d3plus-react.v0.5.js
+++ js/d3plus-react.v0.5.js
@@ -1,5 +1,5 @@
 /*
-  d3plus-react v0.5.2
+  d3plus-react v0.5.3
   React components for d3plus visualizations.
   Copyright (c) 2019 D3plus - https://d3plus.org
   @license MIT
@@ -63,6 +63,147 @@ if (!Array.prototype.includes) {
   });
 }
 
+if (!String.prototype.includes) {
+  Object.defineProperty(String.prototype, 'includes', {
+    value: function(search, start) {
+      if (typeof start !== 'number') {
+        start = 0
+      }
+
+      if (start + search.length > this.length) {
+        return false
+      } else {
+        return this.indexOf(search, start) !== -1
+      }
+    }
+  })
+}
+
+if (!Array.prototype.find) {
+  Object.defineProperty(Array.prototype, 'find', {
+    value: function(predicate) {
+     // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      }
+
+      // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      var thisArg = arguments[1];
+
+      // 5. Let k be 0.
+      var k = 0;
+
+      // 6. Repeat, while k < len
+      while (k < len) {
+        // a. Let Pk be ! ToString(k).
+        // b. Let kValue be ? Get(O, Pk).
+        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+        // d. If testResult is true, return kValue.
+        var kValue = o[k];
+        if (predicate.call(thisArg, kValue, k, o)) {
+          return kValue;
+        }
+        // e. Increase k by 1.
+        k++;
+      }
+
+      // 7. Return undefined.
+      return undefined;
+    },
+    configurable: true,
+    writable: true
+  });
+}
+
+if (!String.prototype.startsWith) {
+  Object.defineProperty(String.prototype, 'startsWith', {
+      value: function(search, pos) {
+          pos = !pos || pos < 0 ? 0 : +pos;
+          return this.substring(pos, pos + search.length) === search;
+      }
+  });
+}
+
+if (typeof window !== "undefined") {
+  (function () {
+    var serializeXML = function (node, output) {
+      var nodeType = node.nodeType;
+      if (nodeType === 3) {
+        output.push(node.textContent.replace(/&/, '&amp;').replace(/</, '&lt;').replace('>', '&gt;'));
+      } else if (nodeType === 1) {
+        output.push('<', node.tagName);
+        if (node.hasAttributes()) {
+          [].forEach.call(node.attributes, function(attrNode){
+            output.push(' ', attrNode.item.name, '=\'', attrNode.item.value, '\'');
+          })
+        }
+        if (node.hasChildNodes()) {
+          output.push('>');
+          [].forEach.call(node.childNodes, function(childNode){
+            serializeXML(childNode, output);
+          })
+          output.push('</', node.tagName, '>');
+        } else {
+          output.push('/>');
+        }
+      } else if (nodeType == 8) {
+        output.push('<!--', node.nodeValue, '-->');
+      }
+    }
+
+    Object.defineProperty(SVGElement.prototype, 'innerHTML', {
+      get: function () {
+        var output = [];
+        var childNode = this.firstChild;
+        while (childNode) {
+          serializeXML(childNode, output);
+          childNode = childNode.nextSibling;
+        }
+        return output.join('');
+      },
+      set: function (markupText) {
+        while (this.firstChild) {
+          this.removeChild(this.firstChild);
+        }
+
+        try {
+          var dXML = new DOMParser();
+          dXML.async = false;
+
+          var sXML = '<svg xmlns=\'http://www.w3.org/2000/svg\' xmlns:xlink=\'http://www.w3.org/1999/xlink\'>' + markupText + '</svg>';
+          var svgDocElement = dXML.parseFromString(sXML, 'text/xml').documentElement;
+
+          var childNode = svgDocElement.firstChild;
+          while (childNode) {
+            this.appendChild(this.ownerDocument.importNode(childNode, true));
+            childNode = childNode.nextSibling;
+          }
+        } catch (e) {};
+      }
+    });
+
+    Object.defineProperty(SVGElement.prototype, 'innerSVG', {
+      get: function () {
+        return this.innerHTML;
+      },
+      set: function (markup) {
+        this.innerHTML = markup;
+      }
+    });
+
+  })();
+}
+
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('react'), require('prop-types'), require('d3plus-common'), require('d3plus-plot'), require('d3plus-hierarchy'), require('d3plus-geomap'), require('d3plus-network'), require('d3plus-priestley')) :
   typeof define === 'function' && define.amd ? define('d3plus-react', ['exports', 'react', 'prop-types', 'd3plus-common', 'd3plus-plot', 'd3plus-hierarchy', 'd3plus-geomap', 'd3plus-network', 'd3plus-priestley'], factory) :
@@ -77,7 +218,7 @@ if (!Array.prototype.includes) {
       @extends React.Component
       @desc Creates SVG paths and coordinate points based on an array of data. See [this example](https://d3plus.org/examples/d3plus-geomap/getting-started/) for help getting started using the geomap generator.
   */
-  var Viz = (function (Component) {
+  var Viz = /*@__PURE__*/(function (Component) {
     function Viz () {
       Component.apply(this, arguments);
     }
@@ -160,7 +301,7 @@ if (!Array.prototype.includes) {
       @class AreaPlot
       @extends Viz
   */
-  var AreaPlot = (function (Shell) {
+  var AreaPlot = /*@__PURE__*/(function (Shell) {
       function AreaPlot () {
           Shell.apply(this, arguments);
       }if ( Shell ) AreaPlot.__proto__ = Shell;
@@ -177,7 +318,7 @@ if (!Array.prototype.includes) {
       @class BarChart
       @extends Viz
   */
-  var BarChart = (function (Shell) {
+  var BarChart = /*@__PURE__*/(function (Shell) {
       function BarChart () {
           Shell.apply(this, arguments);
       }if ( Shell ) BarChart.__proto__ = Shell;
@@ -194,7 +335,7 @@ if (!Array.prototype.includes) {
       @class BumpChart
       @extends Viz
   */
-  var BumpChart = (function (Shell) {
+  var BumpChart = /*@__PURE__*/(function (Shell) {
       function BumpChart () {
           Shell.apply(this, arguments);
       }if ( Shell ) BumpChart.__proto__ = Shell;
@@ -211,7 +352,7 @@ if (!Array.prototype.includes) {
       @class Donut
       @extends Viz
   */
-  var Donut = (function (Shell) {
+  var Donut = /*@__PURE__*/(function (Shell) {
       function Donut () {
           Shell.apply(this, arguments);
       }if ( Shell ) Donut.__proto__ = Shell;
@@ -228,7 +369,7 @@ if (!Array.prototype.includes) {
       @class Geomap
       @extends Viz
   */
-  var Geomap = (function (Shell) {
+  var Geomap = /*@__PURE__*/(function (Shell) {
       function Geomap () {
           Shell.apply(this, arguments);
       }if ( Shell ) Geomap.__proto__ = Shell;
@@ -245,7 +386,7 @@ if (!Array.prototype.includes) {
       @class LinePlot
       @extends Viz
   */
-  var LinePlot = (function (Shell) {
+  var LinePlot = /*@__PURE__*/(function (Shell) {
       function LinePlot () {
           Shell.apply(this, arguments);
       }if ( Shell ) LinePlot.__proto__ = Shell;
@@ -262,7 +403,7 @@ if (!Array.prototype.includes) {
       @class Network
       @extends Viz
   */
-  var Network = (function (Shell) {
+  var Network = /*@__PURE__*/(function (Shell) {
       function Network () {
           Shell.apply(this, arguments);
       }if ( Shell ) Network.__proto__ = Shell;
@@ -279,7 +420,7 @@ if (!Array.prototype.includes) {
       @class Pack
       @extends Viz
   */
-  var Pack = (function (Shell) {
+  var Pack = /*@__PURE__*/(function (Shell) {
       function Pack () {
           Shell.apply(this, arguments);
       }if ( Shell ) Pack.__proto__ = Shell;
@@ -296,7 +437,7 @@ if (!Array.prototype.includes) {
       @class Pie
       @extends Viz
   */
-  var Pie = (function (Shell) {
+  var Pie = /*@__PURE__*/(function (Shell) {
       function Pie () {
           Shell.apply(this, arguments);
       }if ( Shell ) Pie.__proto__ = Shell;
@@ -313,7 +454,7 @@ if (!Array.prototype.includes) {
       @class Plot
       @extends Viz
   */
-  var Plot = (function (Shell) {
+  var Plot = /*@__PURE__*/(function (Shell) {
       function Plot () {
           Shell.apply(this, arguments);
       }if ( Shell ) Plot.__proto__ = Shell;
@@ -330,7 +471,7 @@ if (!Array.prototype.includes) {
       @class Priestley
       @extends Viz
   */
-  var Priestley = (function (Shell) {
+  var Priestley = /*@__PURE__*/(function (Shell) {
       function Priestley () {
           Shell.apply(this, arguments);
       }if ( Shell ) Priestley.__proto__ = Shell;
@@ -347,7 +488,7 @@ if (!Array.prototype.includes) {
       @class Radar
       @extends Viz
   */
-  var Radar = (function (Shell) {
+  var Radar = /*@__PURE__*/(function (Shell) {
       function Radar () {
           Shell.apply(this, arguments);
       }if ( Shell ) Radar.__proto__ = Shell;
@@ -364,7 +505,7 @@ if (!Array.prototype.includes) {
       @class Rings
       @extends Viz
   */
-  var Rings = (function (Shell) {
+  var Rings = /*@__PURE__*/(function (Shell) {
       function Rings () {
           Shell.apply(this, arguments);
       }if ( Shell ) Rings.__proto__ = Shell;
@@ -381,7 +522,7 @@ if (!Array.prototype.includes) {
       @class Sankey
       @extends Viz
   */
-  var Sankey = (function (Shell) {
+  var Sankey = /*@__PURE__*/(function (Shell) {
       function Sankey () {
           Shell.apply(this, arguments);
       }if ( Shell ) Sankey.__proto__ = Shell;
@@ -398,7 +539,7 @@ if (!Array.prototype.includes) {
       @class StackedArea
       @extends Viz
   */
-  var StackedArea = (function (Shell) {
+  var StackedArea = /*@__PURE__*/(function (Shell) {
       function StackedArea () {
           Shell.apply(this, arguments);
       }if ( Shell ) StackedArea.__proto__ = Shell;
@@ -415,7 +556,7 @@ if (!Array.prototype.includes) {
       @class Tree
       @extends Viz
   */
-  var Tree = (function (Shell) {
+  var Tree = /*@__PURE__*/(function (Shell) {
       function Tree () {
           Shell.apply(this, arguments);
       }if ( Shell ) Tree.__proto__ = Shell;
@@ -432,7 +573,7 @@ if (!Array.prototype.includes) {
       @class Treemap
       @extends Viz
   */
-  var Treemap = (function (Shell) {
+  var Treemap = /*@__PURE__*/(function (Shell) {
       function Treemap () {
           Shell.apply(this, arguments);
       }if ( Shell ) Treemap.__proto__ = Shell;
